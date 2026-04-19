0b0ff61ec86dccdf62dc399d2c298fbc8f2b3bfa
Guillaume Brunerie <guillaume.brunerie@gmail.com>
2026-04-19 15:03:31 +0200

    buildings

---
+++ src/app/mapScreen/Building.ts
@@ -0,0 +1,26 @@
+import { Assets, Sprite, type ViewContainerOptions } from "pixi.js";
+import { Container } from "../../PausableContainer";
+
+export class Building extends Container {
+	buildingId: string;
+	constructor(options: ViewContainerOptions) {
+		const buildingIds = [];
+		for (let i = 1; i < 99; i++) {
+			if (
+				Assets.cache.has(`Buildings_${String(i).padStart(2, "0")}.png`)
+			) {
+				buildingIds.push(`Buildings_${String(i).padStart(2, "0")}.png`);
+			}
+		}
+		super();
+		this.buildingId =
+			buildingIds[Math.floor(Math.random() * buildingIds.length)];
+		this.addChild(
+			new Sprite({
+				...options,
+				texture: Assets.get(this.buildingId),
+				anchor: 0.5,
+			}),
+		);
+	}
+}
